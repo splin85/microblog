7f2eeb692f19db8fc785b87d904205599a878ed9
splin85 <splin85@gmail.com>
2018-10-11 21:01:35 +0800

    add template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return 'hello world!'
+    user = { 'nickname' : 'Splin' }
+    return render_template("index.html",
+                           title = "Home",
+                           user  = user['nickname']) 
